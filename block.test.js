292052039670581907edf977c30ff0034bbb3a4b
Shivaot <shivatiwari757@gmail.com>
2019-09-04 11:05:29 +0530

    Test suites for Blocks are created and Passed

--- block.test.js
+++ block.test.js
@@ -1,3 +1,21 @@
 const Block = require('./block');
 
-describe('Block' , ()=> {});
+describe('Block' , ()=> {
+    let data,lastBlock,block;
+
+    beforeEach(() => {
+         data = 'bar';
+         lastBlock = Block.genesis();
+         block = Block.mineBlock(lastBlock,data);
+    });
+
+    it('sets the `data` to match the input',() => {
+        //writing an assertion
+        expect(block.data).toEqual(data);
+    });
+
+    it('sets the lastHash to match the hash of the last block',() => {
+        //writing an assertion
+        expect(block.lastHash).toEqual(lastBlock.hash);
+    });
+});
