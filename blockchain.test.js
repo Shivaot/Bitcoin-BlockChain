3d426fbe2a0a236e30f9c1d4e8bfea756c333754
Shivaot <shivatiwari757@gmail.com>
2019-09-04 14:59:05 +0530

    Test Suites for Chain are added Passed

---
+++ blockchain.test.js
@@ -0,0 +1,22 @@
+const Blockchain = require('./blockchain');
+const Block = require('./block');
+
+describe('Blockchain' , () => {
+    let bc;
+
+    beforeEach(() => {
+        bc = new Blockchain();
+    });
+
+    it('starts with a genesis block', ()=>{
+        //writing an assertion
+        expect(bc.chain[0]).toEqual(Block.genesis());
+    });
+
+    it('adds a new block',()=> {
+        const data = 'foo';
+        bc.addBlock(data);
+
+        expect(bc.chain[bc.chain.length-1].data).toEqual(data);
+    });
+});
