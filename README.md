e7d16971634cb29d3442be8e511833a360e7f4b1
Shivaot <shivatiwari757@gmail.com>
2019-09-04 20:11:43 +0530

    REST-API For Get & Post Created

--- README.md
+++ README.md
@@ -2,10 +2,26 @@ Block chain project in node.js.
 
 This is a simple clone of Bitcoin network in with some new features.
 
+Installation: npm install
+
 Packages required are:
 
-    1.crypto-js
-    
-    2.jest
+    1.crypto-js --> npm i crypto --save
 
-    3.nodemon(optional)
+    2.jest      --> npm i crypto --save
+
+    3.express   --> npm i express --save
+
+    4.body-parser --> npm i body-parser --save
+
+    5.nodemon(optional) --> npm i nodemon
+
+Run :
+
+    - npm run test
+
+    - npm run dev 
+
+        - > localhos:3001/blocks
+
+        - > localhos:3001/mine
---
+++ app/index.js
@@ -0,0 +1,25 @@
+const express = require('express'); //for rest-api
+const bodyParser = require('body-parser');
+const Blockchain = require('../blockchain');
+
+//first part will allow app to run other ports
+const HTTP_PORT = process.env.HTTP_PORT || 3001;
+
+const app = express();
+const bc = new Blockchain();
+
+app.use(bodyParser.json());
+
+app.get('/blocks', (req,res) => {
+    res.json(bc.chain);
+});
+
+app.post('/mine', (req,res) => {
+    const block = bc.addBlock(req.body.data);
+    console.log(`New block added: ${block.toString()}`);
+
+    res.redirect('/blocks');
+});
+
+
+app.listen(HTTP_PORT, () => console.log(`Listening on the port ${HTTP_PORT}`));
